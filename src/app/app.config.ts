22f531d478f3ee52a0460b8663ac77ff05fd32ee
agarwalChirag22 <chirag@vendosmart.com>
2025-05-05 16:30:12 +0530

    prime ng with bootstrap integrate done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations()
+    // Remove providePrimeNG from here
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TestComponentsComponent } from './test-component/test-components/test-components.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'test', component: TestComponentsComponent }
+];
