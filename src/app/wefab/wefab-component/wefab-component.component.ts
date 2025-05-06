861b486ede993c48fe0e6de7421b3279e2fdd4d8
agarwalChirag22 <chirag@vendosmart.com>
2025-05-06 12:50:25 +0530

    Wefab main application and route setup

--- src/app/wefab/wefab-component/wefab-component.component.ts
+++ src/app/wefab/wefab-component/wefab-component.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from '../wefab-shared-component/navigation-bar/navigation-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wefab-component',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent, CommonModule],
   templateUrl: './wefab-component.component.html',
   styleUrl: './wefab-component.component.scss'
 })
