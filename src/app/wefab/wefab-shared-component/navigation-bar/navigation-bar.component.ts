203feb045c8beeb5306c09ef21b3f7de33c4919d
agarwalChirag22 <chirag@vendosmart.com>
2025-05-06 13:04:04 +0530

    Wefab Navigation bar setup

--- src/app/wefab/wefab-shared-component/navigation-bar/navigation-bar.component.ts
+++ src/app/wefab/wefab-shared-component/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgbModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
