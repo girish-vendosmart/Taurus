092e1de7a3ada54b9e39bfe99f9eb13e072a727a
agarwalChirag22 <chirag@vendosmart.com>
2025-05-13 12:25:18 +0530

    get supplier company Id from sessionstorage

--- src/app/wefab/wefab-shared-component/login-component/login-component.component.ts
+++ src/app/wefab/wefab-shared-component/login-component/login-component.component.ts
@@ -62,7 +62,7 @@ export class LoginComponentComponent {
                   
                   // Navigate based on user type
                   if (response.data.user_type === 'supplier') {
-                    this.router.navigate(['/wefab/supplier/supplier-onboarding']);
+                    sessionStorage.setItem('supplier_id', response.data.supplier_info.supplier_company_id);
                     this.router.navigate([response.data.route_link]);
                   } else if (response.data.user_type === 'wefab_team') {
                     this.router.navigate(['/wefab/wefabTeam/dashboard']);
