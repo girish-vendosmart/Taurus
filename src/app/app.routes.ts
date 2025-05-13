3b8241db94d5b5372ea6d54b9751e3fe2e7bb789
agarwalChirag22 <chirag@vendosmart.com>
2025-05-13 19:33:42 +0530

    Common table added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { TestComponentsComponent } from './test-component/test-components/test-c
 import { SupplierOnboardingComponentsTsComponent } from './supplier/supplier-onboarding.components.ts/supplier-onboarding.components.ts.component';
 import { FileExplorerComponent } from './file-explorer/file-explorer.component';
 import { CommonTableComponent } from './wefab/wefab-shared-component/common-table/common-table.component';
+import { CommonPrimeTableComponent } from './wefab/wefab-shared-component/common-prime-table/common-prime-table.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'wefab', pathMatch: 'full' },
-  { path : 'common-table', component : CommonTableComponent},
+  { path : 'common-table', component : CommonPrimeTableComponent},
   // { path: 'test', component: TestComponentsComponent },
   // { path: 'supplier-onboarding', component: SupplierOnboardingComponentsTsComponent },
   // { path: 'file-explorer', component: FileExplorerComponent },
