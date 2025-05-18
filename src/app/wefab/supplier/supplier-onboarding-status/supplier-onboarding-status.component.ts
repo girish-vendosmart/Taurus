c5cfd395b0a68d8f305b979cf6b1a5f30ac340dc
Ubuntu <ubuntu@ip-172-31-42-107.us-west-2.compute.internal>
2025-05-18 07:39:29 +0000

    Supplier onboarding status paged added

--- src/app/wefab/supplier/supplier-onboarding-status/supplier-onboarding-status.component.ts
+++ src/app/wefab/supplier/supplier-onboarding-status/supplier-onboarding-status.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { CommonService } from '../../shared/common.service';
 
 interface OnboardingStep {
   id: number;
@@ -25,7 +26,7 @@ export class SupplierOnboardingStatusComponent {
   onboardingSteps: OnboardingStep[] = [
     {
       id: 1,
-      title: 'Basic Information & Financial Information',
+      title: 'Basic Information',
       description: 'Company details, contact information, and basic financial data',
       status: 'completed',
       progress: 100,
@@ -48,11 +49,113 @@ export class SupplierOnboardingStatusComponent {
       icon: 'lock'
     }
   ];
+  currentOnboardingL1Status: any;
+  currentOnboardingL2Status: any;
+  currentOnboardingL3Status: any;
+  currentOnboardingPage: any;
 
-  constructor(private router: Router) {
-    this.calculateOverallProgress();
+  constructor(private router: Router, private commonService: CommonService) {
+    this.getOnboardingL1Status()
   }
 
+  supplierCompanyId = sessionStorage.getItem('supplier_id')
+
+  getOnboardingL1Status() {
+    let endPoint = `/api/method/proq_buyer.wefab.api.supplier.onboarding.get_onboarding_stage_status?onboarding_stage=L1&supplier_company_id=${this.supplierCompanyId}`
+    this.commonService.getData(endPoint).subscribe((res: any) => {
+      debugger
+      console.log(res)
+      this.currentOnboardingL1Status = res.data.approval_status
+      this.getOnboardingL2Status()
+    })
+  }
+
+  getOnboardingL2Status() {
+    let endPoint = `/api/method/proq_buyer.wefab.api.supplier.onboarding.get_onboarding_stage_status?onboarding_stage=L2&supplier_company_id=${this.supplierCompanyId}`
+    this.commonService.getData(endPoint).subscribe((res: any) => {
+      this.currentOnboardingL2Status = res.data.approval_status
+      this.getOnboardingL3Status()
+    })
+  }
+
+  getOnboardingL3Status() {
+    let endPoint = `/api/method/proq_buyer.wefab.api.supplier.onboarding.get_onboarding_stage_status?onboarding_stage=L3&supplier_company_id=${this.supplierCompanyId}`
+    this.commonService.getData(endPoint).subscribe((res: any) => {
+      this.currentOnboardingL3Status = res.data.approval_status
+      this.underReviewStatus()
+      this.updateOnboardingSteps()
+    })
+  }
+
+  underReviewStatus() {
+    if(this.currentOnboardingL3Status === 'Under Review') {
+      this.onboardingSteps[2].status = 'in-progress'
+      this.onboardingSteps[2].progress = 50
+      this.currentOnboardingPage = 3
+    }
+    else if(this.currentOnboardingL2Status === 'Under Review') {
+      this.onboardingSteps[1].status = 'in-progress'
+      this.onboardingSteps[1].progress = 50
+      this.currentOnboardingPage = 2
+
+    } else if(this.currentOnboardingL1Status === 'Under Review') {
+      this.onboardingSteps[0].status = 'in-progress'
+      this.onboardingSteps[0].progress = 50
+      this.currentOnboardingPage = 1
+    }
+  }
+
+  updateOnboardingSteps() {
+    if(this.currentOnboardingL1Status === 'Not Started') {
+      this.onboardingSteps[0].status = 'pending'
+      this.onboardingSteps[0].progress = 0
+      this.onboardingSteps[0].icon = 'lock'
+    }
+    if(this.currentOnboardingL2Status === 'Not Started') {
+      this.onboardingSteps[1].status = 'pending'
+      this.onboardingSteps[1].progress = 0
+      this.onboardingSteps[1].icon = 'lock'
+    }
+    if(this.currentOnboardingL3Status === 'Not Started') {
+      this.onboardingSteps[2].status = 'pending'
+      this.onboardingSteps[2].progress = 0
+      this.onboardingSteps[2].icon = 'lock'
+    }
+    if(this.currentOnboardingL1Status === 'Under Review') {
+      this.onboardingSteps[0].status = 'in-progress'
+      this.onboardingSteps[0].progress = 50
+      this.onboardingSteps[0].icon = 'clock'
+    }
+    if(this.currentOnboardingL2Status === 'Under Review') {
+      this.onboardingSteps[1].status = 'in-progress'
+      this.onboardingSteps[1].progress = 50 
+      this.onboardingSteps[1].icon = 'clock'
+    }
+    if(this.currentOnboardingL3Status === 'Under Review') {
+      this.onboardingSteps[2].status = 'in-progress'
+      this.onboardingSteps[2].progress = 50
+      this.onboardingSteps[2].icon = 'clock'
+    } 
+    if(this.currentOnboardingL1Status === 'Approved') {
+      this.onboardingSteps[0].status = 'completed'
+      this.onboardingSteps[0].progress = 100
+      this.onboardingSteps[0].icon = 'check'
+      this.currentOnboardingPage = 1
+    }
+    if(this.currentOnboardingL2Status === 'Approved') {
+      this.onboardingSteps[1].status = 'completed'
+      this.onboardingSteps[1].progress = 100  
+      this.onboardingSteps[1].icon = 'check'
+      this.currentOnboardingPage = 2
+    }
+    if(this.currentOnboardingL3Status === 'Approved') {
+      this.onboardingSteps[2].status = 'completed'
+      this.onboardingSteps[2].progress = 100
+      this.onboardingSteps[2].icon = 'check'
+      this.currentOnboardingPage = 3
+    }
+    this.calculateOverallProgress()
+  }
   calculateOverallProgress(): void {
     const totalSteps = this.onboardingSteps.length;
     const completedProgress = this.onboardingSteps.reduce(
@@ -63,14 +166,16 @@ export class SupplierOnboardingStatusComponent {
 
   continueOnboarding(stepId: number): void {
     console.log(`Continuing to step ${stepId}`);
-    this.router.navigate(['/supplier/supplier-onboarding', stepId]);
+    let step = stepId + 1
+    console.log(step)
+    this.router.navigate([`/wefab/supplier/supplier-onboarding-l${step}`]);
   }
 
   viewDetails(stepId: number): void {
     console.log(`Viewing details for step ${stepId}`);
     // Navigate to view the details of the completed step
     // this.router.navigate(['/supplier/onboarding', stepId, 'details']);
-    this.router.navigate(['/supplier/profile-review']);
+    this.router.navigate(['/wefab/supplier/profile-review']);
   }
 
   getStatusLabel(status: string): string {
