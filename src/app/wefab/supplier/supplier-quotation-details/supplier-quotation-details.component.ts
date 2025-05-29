7e899e5fe348db49c035e1a5732459469eda042f
girish-vendosmart <girish.kadli@vendosmart.com>
2025-05-29 09:50:41 +0000

    activity trail integrated

--- src/app/wefab/supplier/supplier-quotation-details/supplier-quotation-details.component.ts
+++ src/app/wefab/supplier/supplier-quotation-details/supplier-quotation-details.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CommonTableComponent, TableConfig, TableColumn, ActionButton } from '../../wefab-shared-component/common-table/common-table.component';
 import { CommonService } from '../../shared/common.service';
+import { ActivityTrailComponent, ActivityLogData } from '../../../common-core-component/activity-trail';
 
 // PrimeNG imports
 import { ButtonModule } from 'primeng/button';
@@ -121,7 +122,8 @@ export interface QuotationItem {
     FormsModule,
     CommonTableComponent,
     ButtonModule,
-    InputTextModule
+    InputTextModule,
+    ActivityTrailComponent
   ],
   templateUrl: './supplier-quotation-details.component.html',
   styleUrl: './supplier-quotation-details.component.scss'
@@ -165,6 +167,9 @@ export class SupplierQuotationDetailsComponent implements OnInit {
   itemsPerPage: number = 7;
   totalItems: number = 0;
   loading: boolean = false;
+
+  activityTrail: ActivityLogData[] = [];
+  activityTrailLoading: boolean = false;
 
   // Table configuration
   tableConfig: TableConfig = {
@@ -359,6 +364,9 @@ export class SupplierQuotationDetailsComponent implements OnInit {
 
   setActiveTab(tab: string): void {
     this.activeTab = tab;
+    if (tab === 'activity') {
+      this.loadActivityTrail();
+    }
   }
 
   onBackToRFQs(): void {
@@ -604,5 +612,20 @@ export class SupplierQuotationDetailsComponent implements OnInit {
       unitPrice: this.formatCurrency(item.unitPrice),
       totalPrice: this.formatCurrency(item.totalPrice)
     }));
+  }
+
+  private loadActivityTrail(): void {
+    this.activityTrailLoading = true;
+    this.commonService.getData('/api/method/proq_buyer.api.core.versioning.get_new_versions_trail?doctype=pq_rfq&docname=' + 'RFQ0000000050')
+      .subscribe({
+        next: (response: any) => {
+          this.activityTrail = response.data || [];
+          this.activityTrailLoading = false;
+        },
+        error: () => {
+          this.activityTrail = [];
+          this.activityTrailLoading = false;
+        }
+      });
   }
 }
