8f33a18de86d65d416ad636379e44f2ea34f6f46
girish-vendosmart <girish.kadli@vendosmart.com>
2025-06-29 17:11:27 +0530

    Fix issue

--- src/app/shared/components/common-table/common-table.component.ts
+++ src/app/shared/components/common-table/common-table.component.ts
@@ -378,8 +378,8 @@ export class CommonTableComponent implements OnInit, AfterViewInit {
     switch (status) {
       case 'Order Complete':
         return 'status-approved';
-      case 'Dispatch':
-        return 'status-in-progress';
+      case 'dispatch':
+        return 'status-progress';
       case 'supplier-confirmation':
         return 'status-open';
       case 'finishing':
