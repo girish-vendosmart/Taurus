f85c0d92e03f6f3beca7fbe234531580fd47ba2b
girish-vendosmart <girish.kadli@vendosmart.com>
2025-07-05 19:00:06 +0530

    Enviornment prod ready

---
+++ src/enviornments/environment.customer.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://fabster.wefab.ai', // Production API URL
+  wefabApiUrl: 'https://fabster.wefab.ai', // Production WEFAB API URL
+  isCustomerBranch: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDcYuSy6cy-RrJAHKl1xIqmRmC9yb-r5Dw",
+    authDomain: "esourcing-trial.firebaseapp.com",
+    projectId: "esourcing-trial",
+    storageBucket: "esourcing-trial.firebasestorage.app",
+    messagingSenderId: "943655992390",
+    appId: "1:943655992390:web:3980aee70926f1a62dde47",
+    measurementId: "G-8BDZQ7Z5PC"
+  }
+} 
