ad894906b50b579f0262f989c9fde909bfeb85b2
girish-vendosmart <girish.kadli@vendosmart.com>
2025-07-06 02:10:38 +0530

    Fix issue

--- src/app/wefab/customer/quotation/quotation-list/quotation-list.component.ts
+++ src/app/wefab/customer/quotation/quotation-list/quotation-list.component.ts
@@ -135,7 +135,7 @@ export class QuotationListComponent implements OnInit {
  
   getQuotationList() {
     this.loading = true;
-    let apiEndpoint = '/api/resource/Wefab Quotation?fields=["*"]&filters=[["status","!=","Draft"]]';
+    let apiEndpoint = '/api/resource/Wefab Quotation?fields=["*"]&filters=[["status","!=","Draft"]]&limit=400&order_by=creation desc';
     this.commonService.getData(apiEndpoint).subscribe({
       next: (res: any) => {
         this.quotationData = res.data.map((quotation: any) => ({
